971394279c2bf1add64c0a92b6114a4e7fa493b6
reuben-k-o <rubenkhaemba@gmail.com>
2022-04-20 16:33:48 +0300

    signup functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
+const bcrypt = require("bcrypt");
 const { Router } = require("express");
 
 const router = Router();
+
+const users = [];
+
 router.get("/", (req, res, next) => {
   res.status(200).render("index", { name: "Reuben" });
 });
@@ -8,7 +12,20 @@ router.get("/", (req, res, next) => {
 router.get("/signup", (req, res, next) => {
   res.status(200).render("signup");
 });
-router.post("/signup", (req, res, next) => {});
+router.post("/signup", async (req, res, next) => {
+  try {
+    const hashedPass = await bcrypt.hash(req.body.password, 12);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPass,
+    });
+    console.log(users);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 router.get("/login", (req, res, next) => {
   res.status(200).render("login");
