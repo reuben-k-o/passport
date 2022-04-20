f24b07cc73f73ae41f814d42c503be61f0bf5190
reuben-k-o <rubenkhaemba@gmail.com>
2022-04-20 16:11:48 +0300

    basic

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(authRoutes);
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,18 @@
 const { Router } = require("express");
 
 const router = Router();
+router.get("/", (req, res, next) => {
+  res.status(200).render("index", { name: "Reuben" });
+});
 
-router.get("/signup", (req, res, next) => {});
+router.get("/signup", (req, res, next) => {
+  res.status(200).render("signup");
+});
+router.post("/signup", (req, res, next) => {});
+
+router.get("/login", (req, res, next) => {
+  res.status(200).render("login");
+});
+router.post("/login", (req, res, next) => {});
 
 module.exports = router;
